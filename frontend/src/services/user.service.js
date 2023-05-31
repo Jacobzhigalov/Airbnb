da0bfa974f1ce285f8d9de0bb382d9c4665b8398
Marina Milman <shkrechok@gmail.com>
2023-05-31 19:45:19 +0300

    user service adjusted to local storage

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -43,6 +43,7 @@ const demoUsers = [
       }
   ]
 
+  _createUsers()
 export const userService = {
     login,
     logout,
@@ -60,27 +61,27 @@ window.userService = userService
 
 
 function getUsers() {
-    return storageService.query('user')
+    return storageService.query(STORAGE_KEY)
     // return httpService.get(`user`)
 }
 
 
 
 async function getById(userId) {
-    const user = await storageService.get('user', userId)
+    const user = await storageService.get(STORAGE_KEY, userId)
     // const user = await httpService.get(`user/${userId}`)
     return user
 }
 
 function remove(userId) {
-    return storageService.remove('user', userId)
+    return storageService.remove(STORAGE_KEY, userId)
     // return httpService.delete(`user/${userId}`)
 }
 
 async function update({_id, score}) {
-    const user = await storageService.get('user', _id)
+    const user = await storageService.get(STORAGE_KEY, _id)
     user.score = score
-    await storageService.put('user', user)
+    await storageService.put(STORAGE_KEY, user)
 
     // const user = await httpService.put(`user/${_id}`, {_id, score})
     // Handle case in which admin updates other user's details
@@ -89,7 +90,7 @@ async function update({_id, score}) {
 }
 
 async function login(userCred) {
-    const users = await storageService.query('user')
+    const users = await storageService.query(STORAGE_KEY)
     const user = users.find(user => user.username === userCred.username)
     // const user = await httpService.post('auth/login', userCred)
     if (user) {
@@ -99,7 +100,7 @@ async function login(userCred) {
 async function signup(userCred) {
     userCred.score = 10000
     if (!userCred.imgUrl) userCred.imgUrl = 'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png'
-    const user = await storageService.post('user', userCred)
+    const user = await storageService.post(STORAGE_KEY, userCred)
     // const user = await httpService.post('auth/signup', userCred)
     return saveLocalUser(user)
 }

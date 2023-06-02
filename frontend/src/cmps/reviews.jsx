ded365ceae8e2a119df0bf75eaf2124711d6ccd7
David Ohr <our.david96@gmail.com>
2023-06-02 14:43:02 +0300

    UPDATE reviews style

--- frontend/src/cmps/reviews.jsx
+++ frontend/src/cmps/reviews.jsx
@@ -1,16 +1,27 @@
 // IMPORT {React.Fragment} from React
 import React from 'react';
-export function Reviews({stay}){
+export function Reviews({ stay }) {
+    function getTxt(review) {
+        const txt = (review.txt.length < 150) ? review.txt : getLongTxt(review)
+        return txt
+    }
+    function getLongTxt(review) {
+        return `${review.txt.substr(0, 150)}...`
+    }
+    function showReviews() {
+        alert('under construction')
+    }
 
-    return(<React.Fragment>
-            {stay.reviews.map(review=>{
-                return<div className="review">
-                    
-                     <h3><img src={review.by.imgUrl}/>{ review.by.fullname}</h3> <br/>
-                     {review.txt}
-
-                    </div>
-            })}
-            </React.Fragment>
+    return (<React.Fragment>
+        {stay.reviews.map(review => {
+            return <div className="review">
+                <img src={review.by.imgUrl} />
+                <h6>{review.by.fullname}</h6> <br />
+                {getTxt(review)}
+                <br />
+                <button onClick={showReviews}>Show more</button> 
+            </div>
+        })}
+    </React.Fragment>
     )
 }

fe428a7e5a0575e75195ee6d0d9ebed7d97d9bb2
David Ohr <our.david96@gmail.com>
2023-06-02 11:52:06 +0300

    UPDATE hedline photo grid and host details design

--- frontend/src/pages/stay-details.jsx
+++ frontend/src/pages/stay-details.jsx
@@ -1,7 +1,7 @@
 import { Link, useNavigate, useParams, useSearchParams } from "react-router-dom"
 import { useEffect, useState } from "react"
 import { stayService } from "../services/stay.service.local.js"
-import {Reviews} from "../cmps/reviews.jsx"
+import { Reviews } from "../cmps/reviews.jsx"
 
 export function StayDetails() {
     const { stayId } = useParams()
@@ -11,54 +11,64 @@ export function StayDetails() {
     }, [stayId])
 
     function loadStay() {
-        
+
         stayService.getById(stayId)
             .then((stay) => {
                 console.log(stay)
-                setStay(stay)})
-            // .catch((err) => {
-            //     console.log('Had issues in stay details', err)
-            //     showErrorMsg('Cannot load stay')
-            //     navigate('/stay')
-            // })
+                setStay(stay)
+            })
+        // .catch((err) => {
+        //     console.log('Had issues in stay details', err)
+        //     showErrorMsg('Cannot load stay')
+        //     navigate('/stay')
+        // })
     }
-console.log(stay)
+    function underConstruction() {
+        alert('This button is under construction. We are sorry for the inconvenience. Please try again later')
+    }
+    console.log(stay)
     if (!stay) return <div>Loading...</div>
     return (
-        <section>
+        <section className="stay-details">
             <header className="details-header">
                 <h4>{stay.name}</h4>
                 <div className="flex space-between">
-               <h6> {stay.loc.country},{stay.loc.city}* reviews:{stay.reviews.length} * avrage rate:{(stay.reviews.reduce((acc,review)=>review.rate+acc,0))/stay.reviews.length}
-               </h6>
-               <h6 >save to favorites share</h6>
-               </div>
+                    <h6> <i class="fa-sharp fa-solid fa-star"></i> <span>{(stay.reviews.reduce((acc, review) => review.rate + acc, 0)) / stay.reviews.length}</span>  · <span> {stay.reviews.length}  reviews </span> · <span>{stay.loc.country},{stay.loc.city}</span>
+                    </h6>
+                    <div>
+                        <button onClick={underConstruction}><i class="fa-solid fa-arrow-up-from-bracket"></i> <sapn>share </sapn></button>
+                        <button onClick={underConstruction}><i class="fa-regular fa-heart"></i> <sapn>save</sapn> </button>
+                    </div>
+                </div>
             </header>
 
             <div className="details-photo-gallery">
-            {stay.imgUrls.map(imgUrl=>{
-            return<img src={imgUrl}></img>
-            })}
+                {stay.imgUrls.map(imgUrl => {
+                    return <img src={imgUrl}></img>
+                })}
             </div>
-
-            <div className="host-details">those are the host details
-            a{stay.type} by {stay.host.fullname} <br/>
-            {stay.capacity} Guests, ZZZZ bed-rooms,ZZZZ beds, ZZZZbath-rooms
-            <hr/>
-            {stay.labels[0]}
-            <br/>
-            {stay.labels[1]}
-            </div>
-
-            <div className="home-details">
-                {stay.summary}
+            < div className="container">
+                <div className="host-details">
+                  <h4>  {stay.type} by {stay.host.fullname} </h4> <br />
+                  {/* <img src={stay.host.imgUrl}/> */}
+                  <img src="https://res.cloudinary.com/dtgdzulrf/image/upload/v1685694692/Stay.si/home0/person-donald-900x1080_cvrpzk.jpg"/> 
+                   <h6> {stay.capacity} Guests · ZZZZ bed-rooms · ZZZZ beds · ZZZZbath-roomszzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz </h6>
+                    <hr />
+                    {stay.labels[0]}
+                    <br />
+                    {stay.labels[1]}
                 </div>
-                <div className="calendare">this is calendare</div>
-                <div className="details-reviews"><Reviews stay={stay}/></div>
-                <div className="map">here coms the map</div>
-                <div className="vital-info">last but not least here is the vital info</div>
-        <img src={stay.host.imgUrl} />
-        <h6>{stay.amenities.map(amn=>`${amn}, `)}</h6>
+
+                <div className="home-details">
+                    {stay.summary}
+                </div>
+            </div>
+            <div className="calendare">this is calendare</div>
+            <div className="details-reviews"><Reviews stay={stay} /></div>
+            <div className="map">here coms the map</div>
+            <div className="vital-info">last but not least here is the vital info</div>
+            <img src={stay.host.imgUrl} />
+            <h6>{stay.amenities.map(amn => `${amn}, `)}</h6>
         </section>
     )
 }

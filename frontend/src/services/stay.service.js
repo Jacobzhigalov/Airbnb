9f7960fb1fb4750d6d92b1cacd1310bdd8a99c22
David Ohr <our.david96@gmail.com>
2023-06-04 17:16:42 +0300

    UPDATE

--- frontend/src/services/stay.service.js
+++ frontend/src/services/stay.service.js
@@ -1,8 +1,7 @@
 
 // import { storageService } from './async-storage.service.js'
 import { httpService } from './http.service.js'
-import { utilService } from './util.service.js'
-import { userService } from './user.service.js'
+// import { userService } from './user.service.js'
 
 
 const STORAGE_KEY = 'stay'
@@ -13,7 +12,8 @@ export const stayService = {
     save,
     remove,
     getEmptyStay,
-    addStayMsg
+    addStayMsg,
+    getDefaultFilter
 }
 window.cs = stayService
 
@@ -41,96 +41,62 @@ async function save(stay) {
 }
 
 async function addStayMsg(stayId, txt) {
-    const savedMsg = await httpService.post(`stay/${stayId}/msg`, {txt})
+    const savedMsg = await httpService.post(`stay/${stayId}/msg`, { txt })
     return savedMsg
 }
 
+function getDefaultFilter() {
+    return { price: 750, txt: '', location: '', checkIn: '', checkOut: '', adults: 0, children: 0, infants: 0, pets: 0 }
+}
 
 function getEmptyStay() {
     return {
-      "_id": "s103",
-      "name": "Mountain Retreat",
-      "type": "Cabin",
-      "imgUrls": ["https://picsum.photos/id/164/200/200",
-        "https://res.cloudinary.com/dtgdzulrf/image/upload/v1685607214/cld-sample-2.jpg",
-        "https://res.cloudinary.com/dtgdzulrf/image/upload/v1685607215/cld-sample-4.jpg",
-        "https://res.cloudinary.com/dtgdzulrf/image/upload/v1685608646/Stay.si/home0/house_on_nqdcal.jpg",
-        "https://res.cloudinary.com/dtgdzulrf/image/upload/v1685608646/Stay.si/home0/binary-4--583f06853df78c6f6a9e0b7a_jljg99.jpg"],
-      "price": 100.00,
-      "summary": "Escape to the peaceful mountainside...",
-      "capacity": 6,
-      "dates": "July 9-14",
-      "rating": "★4.8",
-      "amenities": [
-        "TV",
-        "Wifi",
-        "Kitchen",
-        "Fireplace",
-        "Hiking trails",
-        "Pet-friendly"
-      ],
-      "labels": [
-        "Amazing views",
-        "Nature",
-        "Adventure",
-        "Cozy"
-      ],
-      "host": {
-        "_id": "u105",
-        "fullname": "Emily Johnson",
-        "imgUrl": "https://a0.muscache.com/im/pictures/f5676d2f-0497-4746-9f43-d6c4781edc63.jpg?aki_policy=profile_small"
-      },
-      "loc": {
-        "country": "United States",
-        "countryCode": "US",
-        "city": "Asheville",
-        "address": "456 Mountain Lane",
-        "lat": 35.5951,
-        "lng": -82.5515
-      },
-      "reviews": [
-        {
-          "id": "reviewId7",
-          "txt": "Our family had a fantastic time at this vacation house. The hosts were friendly and accommodating, and the house was perfect for our needs. It was clean, spacious, and had a charming atmosphere. The traditional meals were a highlight of our stay. We can't wait to return!",
-          "rate": 5,
-          "by": {
-            "_id": "userId8",
-            "fullname": "Sophia Brown",
-            "imgUrl": "https://res.cloudinary.com/dtgdzulrf/image/upload/v1685698621/Stay.si/home0/Africa-2_mdcipt.png"
-          }
+        // name: "Ribeira Charming Duplex",
+        type: "House",
+        imgUrls: ["https://image.cnbcfm.com/api/v1/image/106758801-1603459526384-picture-perfect-beautiful-house-on-the-island-of-coronado-in-sunny-california-beautifully-landscaped_t20_6lJOrv.jpg?v=1603459593&w=740&h=416&ffmt=webp&vtcrop=y"],
+        // price: 80.00,
+        summary: "Fantastic duplex apartment...",
+        capacity: 8,
+        amenities: [
+            "TV",
+            "Wifi",
+            "Kitchen",
+            "Smoking allowed",
+            "Pets allowed",
+            "Cooking basics"
+        ],
+        labels: [
+            "Top of the world",
+            "Trending",
+            "Play",
+            "Tropical"
+        ],
+        host: {
+            _id: "u101",
+            fullname: "Davit Pok",
+            imgUrl: "https://a0.muscache.com/im/pictures/fab79f25-2e10-4f0f-9711-663cb69dc7d8.jpg?aki_policy=profile_small",
         },
-        {
-          "id": "reviewId8",
-          "txt": "We couldn't have asked for a better vacation house. The hosts were attentive and made sure we had a wonderful time. The house was beautifully decorated and had all the amenities we needed. The traditional meals were absolutely delicious. We will definitely be back!",
-          "rate": 5,
-          "by": {
-            "_id": "userId9",
-            "fullname": "Matthew Clark",
-            "imgUrl": "https://res.cloudinary.com/dtgdzulrf/image/upload/v1685698622/Stay.si/home0/p-5_mcjkh1.jpg"
-          }
+        loc: {
+            country: "Portugal",
+            countryCode: "PT",
+            city: "Lisbon",
+            address: "17 Kombo st",
+            lat: -8.61308,
+            lng: 41.1413
         },
-        {
-          "id": "reviewId9",
-          "txt": "Our stay at this vacation house was nothing short of amazing. The hosts were gracious and made us feel like part of their family. The house itself was cozy and had breathtaking views. The traditional meals prepared by the hosts were a true culinary delight. We can't recommend this place enough!",
-          "rate": 5,
-          "by": {
-            "_id": "userId10",
-            "fullname": "Olivia Turner",
-            "imgUrl": "https://res.cloudinary.com/dtgdzulrf/image/upload/v1685698622/Stay.si/home0/header-african-american-health-equity-500x500-1_ddcazu.jpg"
-          }
-        },
-        {
-          "id": "reviewId10",
-          "txt": "We had a fantastic experience at this vacation house. The hosts were friendly, helpful, and made our stay unforgettable. The house was spacious, clean, and had a homely atmosphere. The traditional meals were a highlight of our trip. We can't wait to return and create more memories!",
-          "rate": 5,
-          "by": {
-            "_id": "userId11",
-            "fullname": "William Walker",
-            "imgUrl": "https://res.cloudinary.com/dtgdzulrf/image/upload/v1685698621/Stay.si/home0/107228941-1682027700192-_DSC5658_lnioog.jpg"
-          }
-        }
-      ],
-      "likedByUsers": ["user1", "user2", "user3"],
+        reviews: [
+            {
+                id: "madeId",
+                txt: "Very helpful hosts. Cooked traditional...",
+                rate: 4,
+                by: {
+                    _id: "u102",
+                    fullname: "user2",
+                    imgUrl: "/img/img2.jpg"
+                }
+            }
+        ],
+        likedByUsers: ['mini-user']
     }
 }
 

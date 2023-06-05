b247348e762dcf73b462db3d4bc229c3bcd00d13
“JacobZhigalov” <jzhigalov@gmail.com>
2023-06-05 15:12:42 +0300

    sticky labels

--- frontend/src/cmps/label-filter.jsx
+++ frontend/src/cmps/label-filter.jsx
@@ -48,32 +48,37 @@ export function LabelFilter({ onLabelChange }) {
         //     ))}
         // </div>
 
-        <Carousel
-            swipeable={false}
-            draggable={false}
-            // showDots={true}
-            responsive={responsive}
-            ssr={true} // means to render carousel on server-side.
-            // infinite={true}
-            // autoPlay={this.props.deviceType !== "mobile" ? true : false}
-            autoPlaySpeed={1000}
-            keyBoardControl={true}
-            customTransition="all .5"
-            transitionDuration={500}
-            containerClass="carousel-container"
-            // removeArrowOnDeviceType={["tablet", "mobile"]}
-            // deviceType={this.props.deviceType}
-            dotListClass="custom-dot-list-style"
-            itemClass="carousel-item"
-        >
-            {/* <div className="label-filter"> */}
+        <div className='labels-fixed'>
+
+
+
+            <Carousel
+                swipeable={false}
+                draggable={false}
+                // showDots={true}
+                responsive={responsive}
+                ssr={true} // means to render carousel on server-side.
+                // infinite={false}
+                // autoPlay={this.props.deviceType !== "mobile" ? true : false}
+                autoPlaySpeed={1000}
+                keyBoardControl={true}
+                customTransition="all .5"
+                transitionDuration={500}
+                containerClass="carousel-container"
+                // removeArrowOnDeviceType={["tablet", "mobile"]}
+                // deviceType={this.props.deviceType}
+                dotListClass="custom-dot-list-style"
+                itemClass="carousel-item"
+            >
+                {/* <div className="label-filter"> */}
                 {labels.map(label => (
                     <div className='label-container' key={label.title} onClick={() => onLabelChange(label.title)}>
                         <img src={label.url} alt={label.title} />
                         <p>{label.title}</p>
                     </div>
                 ))}
-            {/* </div> */}
-        </Carousel>
+                {/* </div> */}
+            </Carousel>
+        </div>
     )
 }

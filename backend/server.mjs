ea71c0296a32c949c6bf46a8e7a420d96f25bf69
David Ohr <our.david96@gmail.com>
2023-06-08 18:14:15 +0300

    backend fully functional

--- backend/server.mjs
+++ backend/server.mjs
@@ -29,21 +29,22 @@ if (process.env.NODE_ENV === 'production') {
 import { authRoutes } from './api/auth/auth.routes.mjs'
 import { userRoutes } from './api/user/user.routes.mjs'
 import { reviewRoutes } from './api/review/review.routes.mjs'
-import { carRoutes } from './api/car/car.routes.mjs'
+import { stayRoutes } from './api/stay/stay.routes.mjs'
 import { setupSocketAPI } from './services/socket.service.mjs'
 
 // routes
 import { setupAsyncLocalStorage } from './middlewares/setupAls.middleware.mjs'
 app.all('*', setupAsyncLocalStorage)
 
+app.use('/api/order',orderRoutes)
 app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/review', reviewRoutes)
-app.use('/api/car', carRoutes)
+app.use('/api/stay', stayRoutes)
 setupSocketAPI(server)
 
 // Make every server-side-route to match the index.html
-// so when requesting http://localhost:3030/index.html/car/123 it will still respond with
+// so when requesting http://localhost:3030/index.html/stay/123 it will still respond with
 // our SPA (single page app) (the index.html file) and allow vue/react-router to take it from there
 app.get('/**', (req, res) => {
     res.sendFile(path.resolve('public/index.html'))
@@ -51,6 +52,7 @@ app.get('/**', (req, res) => {
 
 
 import { logger } from './services/logger.service.mjs'
+import { orderRoutes } from './api/order/order.routes.mjs'
 const port = process.env.PORT || 3030
 server.listen(port, () => {
     logger.info('Server is running on port: ' + port)

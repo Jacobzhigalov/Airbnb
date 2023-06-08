b481a97e1b296791bd023fcbbc6aee0923c2118d
“JacobZhigalov” <jzhigalov@gmail.com>
2023-06-08 13:11:09 +0300

    filter fixed

--- frontend/src/cmps/header-filter-modal.jsx
+++ frontend/src/cmps/header-filter-modal.jsx
@@ -50,35 +50,39 @@ export default function HeaderFilterModal({ filterByToEdit, handleGuestsChange, 
             {(selectedMenu === 'where') && (
                 <section className="where">
                     <section className="places">
-               {places && places.map(place => <div className="place" key={place} onClick={()=>handleChange({ target: { name: 'where', value: place,type:'text' } })}>{place}</div>)}
-
+                        {places && places.map(place =>
+                            <div className='places-grid'>
+                                <img src={require('../assets/img/jpeg/location.png')} alt="" />
+                                <div className="place" key={place} onClick={() => handleChange({ target: { name: 'where', value: place, type: 'text' } })}>{place}</div>
+                            </div>).slice(0,5)
+                        }
                     </section>
                     <section className="some-mini-maps">
-                        <div className="mini-maps-title">Serch by region</div>
+                        <div className="mini-maps-title">Search by region</div>
                         <div className="mini-maps-container">
                             <div className="mini-map flexible">
                                 <span>Flexible</span>
-                                <img src={require('../assets/img/webp/flexible.jpeg')} alt="flexible pic" onClick={()=>handleChange({ target: { name: 'where', value: '',type:'text' } })} ></img>
+                                <img src={require('../assets/img/webp/flexible.jpeg')} alt="flexible pic" onClick={() => handleChange({ target: { name: 'where', value: '', type: 'text' } })} ></img>
                             </div>
                             <div className="mini-map Italy">
                                 <span>Italy</span>
-                                <img src={require('../assets/img/webp/Italy.webp')} alt="Italy pic" onClick={()=>handleChange({ target: { name: 'where', value: 'Italy',type:'text' } })}></img>
+                                <img src={require('../assets/img/webp/Italy.webp')} alt="Italy pic" onClick={() => handleChange({ target: { name: 'where', value: 'Italy', type: 'text' } })}></img>
                             </div>
                             <div className="mini-map France">
                                 <span>France</span>
-                                <img src={require('../assets/img/webp/France.webp')} alt="France pic" onClick={()=>handleChange({ target: { name: 'where', value: 'France',type:'text' } })} ></img>
+                                <img src={require('../assets/img/webp/France.webp')} alt="France pic" onClick={() => handleChange({ target: { name: 'where', value: 'France', type: 'text' } })} ></img>
                             </div>
                             <div className="mini-map Greece">
                                 <span>United States</span>
-                                <img src={require('../assets/img/webp/Greece.webp')} alt="United States pic" onClick={()=>handleChange({ target: { name: 'where', value: 'United States',type:'text' } })} ></img>
+                                <img src={require('../assets/img/webp/Greece.webp')} alt="United States pic" onClick={() => handleChange({ target: { name: 'where', value: 'United States', type: 'text' } })} ></img>
                             </div>
                             <div className="mini-map Middle East">
                                 <span>Middle East</span>
-                                <img src={require('../assets/img/webp/Middle East.webp')} alt="Middle East pic" onClick={()=>handleChange({ target: { name: 'where', value: 'Middle East',type:'text' } })} ></img>
+                                <img src={require('../assets/img/webp/Middle East.webp')} alt="Middle East pic" onClick={() => handleChange({ target: { name: 'where', value: 'Middle East', type: 'text' } })} ></img>
                             </div>
                             <div className="mini-map South America">
                                 <span>South America</span>
-                                <img src={require('../assets/img/webp/South America.webp')} alt="South America pic" onClick={()=>handleChange({ target: { name: 'where', value: 'South America',type:'text' } })} ></img>
+                                <img src={require('../assets/img/webp/South America.webp')} alt="South America pic" onClick={() => handleChange({ target: { name: 'where', value: 'South America', type: 'text' } })} ></img>
                             </div>
                         </div>
                     </section>
@@ -114,37 +118,45 @@ export default function HeaderFilterModal({ filterByToEdit, handleGuestsChange, 
                             Adults
                         </label>
                         <section className="adults-count-container">
-                            <span className="plus-adult" name="adults" onClick={(ev) => onGuestsChange(ev, 1)}>+</span>
                             {/* () => handleGuestsChange({ target: { name: 'adults', value: filterByToEdit.guests.adults + 1 } }) */}
+                            {+filterByToEdit.guests.adults > 0 &&
+                                <span className="minus-adult" name="adults" onClick={(ev) => onGuestsChange(ev, -1)}>-</span>
+                            }
                             <span className="adults">{+filterByToEdit.guests.adults}</span>
-                            <span className="minus-adult" name="adults" onClick={(ev) => onGuestsChange(ev, -1)}>-</span>
+                            <span className="plus-adult" name="adults" onClick={(ev) => onGuestsChange(ev, 1)}>+</span>
                         </section>
                     </section>
 
                     <section className="children-container">
                         <label htmlFor="children">Children</label>
                         <section className="children-count-container">
-                            <span className="plus-children" name="children" onClick={(ev) => onGuestsChange(ev, 1)}>+</span>
+                            {+filterByToEdit.guests.children > 0 &&
+                                <span className="minus-children" name="children" onClick={(ev) => onGuestsChange(ev, -1)}>-</span>
+                            }
                             <span className="children">{+filterByToEdit.guests.children}</span>
-                            <span className="minus-children" name="children" onClick={(ev) => onGuestsChange(ev, -1)}>-</span>
+                            <span className="plus-children" name="children" onClick={(ev) => onGuestsChange(ev, 1)}>+</span>
                         </section>
                     </section>
 
                     <section className="infants-container">
                         <label htmlFor="infants">Infants</label>
                         <section className="infants-count-container">
-                            <span className="plus-infants" name="infants" onClick={(ev) => onGuestsChange(ev, 1)}>+</span>
+                            {+filterByToEdit.guests.infants > 0 &&
+                                <span className="minus-infants" name="infants" onClick={(ev) => onGuestsChange(ev, -1)}>-</span>
+                            }
                             <span className="infants">{+filterByToEdit.guests.infants}</span>
-                            <span className="minus-infants" name="infants" onClick={(ev) => onGuestsChange(ev, -1)}>-</span>
+                            <span className="plus-infants" name="infants" onClick={(ev) => onGuestsChange(ev, 1)}>+</span>
                         </section>
                     </section>
 
                     <section className="pets-container">
                         <label htmlFor="pets">Pets</label>
                         <section className="pets-count-container">
-                            <span className="plus-pets" name="pets" onClick={(ev) => onGuestsChange(ev, 1)}>+</span>
+                            {filterByToEdit.guests.pets > 0 &&
+                                <span className="minus-pets" name="pets" onClick={(ev) => onGuestsChange(ev, -1)}>-</span>
+                            }
                             <span className="pets">{+filterByToEdit.guests.pets}</span>
-                            <span className="minus-pets" name="pets" onClick={(ev) => onGuestsChange(ev, -1)}>-</span>
+                            <span className="plus-pets" name="pets" onClick={(ev) => onGuestsChange(ev, 1)}>+</span>
                         </section>
                     </section>
                 </section>

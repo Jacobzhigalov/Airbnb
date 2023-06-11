faf081760023f22d95f809e07bf4fe6241fff588
David Ohr <our.david96@gmail.com>
2023-06-11 23:51:52 +0300

    FEATURE start of amenities modal

--- frontend/src/cmps/stay-amenities.jsx
+++ frontend/src/cmps/stay-amenities.jsx
@@ -1,8 +1,11 @@
 import { utilService } from "../services/util.service.js"
-
+import React, { useState } from 'react';
 
 export function StayAmenities({ stay }) {
-
+    const [isModalOpen, setIsModalOpen] = useState(false)
+    function showAmenities() {
+        alert('under construction')
+    }
     // console.log('hello',utilService.getIcon(stay.amenities[0]))
 
     return (
@@ -17,12 +20,32 @@ export function StayAmenities({ stay }) {
                             <p>
                                 {amenity}
                             </p>
+
                         </li>
                     </div>
-                )
-                }
-
+                )}
             </ul>
-        </section>
+            <button onClick={() => setIsModalOpen(true)}>Show all {stay.amenities.length} amenities</button>
+            {<div className="modal-background">
+                <div className="modal-amenities">
+                    <button onClick={() => setIsModalOpen(false)}> close </button>
+                    <h2>What this place offers</h2>
+                    <ul>
+                        {stay.amenities.map(amenity =>
+                            <div>
+                                <li key={amenity}>
+                                    <img src={utilService.getIcon(amenity)} alt="smth" />
+                                    <p>
+                                        {amenity}
+                                    </p>
+                                    <hr className="hLine" />
+                                </li>
+                            </div>
+
+                        )}
+                    </ul>
+                </div>
+            </div>}
+        </section >
     )
 }

06cedd1575b6b016baba75c740f8934448826269
David Ohr <our.david96@gmail.com>
2023-06-14 13:52:39 +0300

    bugfix

--- frontend/src/pages/stay-details.jsx
+++ frontend/src/pages/stay-details.jsx
@@ -50,7 +50,6 @@ export function StayDetails() {
     if (!stay) return <img className="loader" src={loader} />
     return (
         <section className="main-layout-stayDetails">
-            <hr className="hLine"/>
 
             <section className="stay-details">
                 <header className="details-header">

2769915dc3d89bacd4442ab9cde024f7f92a07c9
David Ohr <our.david96@gmail.com>
2023-06-14 12:14:05 +0300

    FIX broken pictures fixed

--- frontend/src/cmps/reviews.jsx
+++ frontend/src/cmps/reviews.jsx
@@ -1,6 +1,6 @@
 // IMPORT {React.Fragment} from React
-import subimage from'../assets/img/user.png'
-import React,{useState} from 'react';
+import subimage from '../assets/img/user.png'
+import React, { useState } from 'react';
 export function Reviews({ stay }) {
     const months = [
         'January',
@@ -15,18 +15,30 @@ export function Reviews({ stay }) {
         'October',
         'November',
         'December'
-      ];
+    ];
     const [updatedReviews, setUpdatedReviews] = useState(stay.reviews);
     const [isModalOpen, setIsModalOpen] = useState(false)
-    const replacementImages=[
+    const replacementImages = [
         subimage
     ]
-    function handleImageError(index){
+    
+    async function handleImageError(index) {
         const updatedReviewsCopy = [...updatedReviews]
-        const replacementImage = replacementImages[Math.floor(Math.random() * replacementImages.length)]
-        updatedReviewsCopy[index].by.imgUrl = replacementImage
+
+        const prms = fetch(`https://api.genderize.io/?name=${updatedReviewsCopy[index].by.fullname}`).then(
+            res => res.json())
+        const res = await Promise.resolve(prms)
+        updatedReviewsCopy[index].by.imgUrl=`https://xsgames.co/randomusers/assets/avatars/${
+            res.gender ? res.gender : 'female'
+        }/${getRandomIntInclusive(0, 78)}.jpg`
+        console.log(res)
         setUpdatedReviews(updatedReviewsCopy)
     }
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min)}
+
     function getTxt(review) {
         const txt = (review.txt.length < 150) ? review.txt : getLongTxt(review)
         return txt
@@ -37,17 +49,17 @@ export function Reviews({ stay }) {
     function showReviews() {
         alert('under construction')
     }
-    function getTime(timestamp){
-        const date=new Date(timestamp)
-        const month =months[date.getMonth()]
+    function getTime(timestamp) {
+        const date = new Date(timestamp)
+        const month = months[date.getMonth()]
         const year = date.getFullYear()
         return `${month} ${year}`
     }
 
     return (<React.Fragment>
-        {updatedReviews.slice(0, 6).map((review,index) => {
+        {updatedReviews.slice(0, 6).map((review, index) => {
             return <div className="review">
-                <img src={review.by.imgUrl} onError={()=>handleImageError(index)}  />
+                <img src={review.by.imgUrl} onError={() => handleImageError(index)} />
                 <h4>{review.by.fullname}</h4>
                 <br />
                 <h5>{getTime(review.at)}</h5>
@@ -58,43 +70,43 @@ export function Reviews({ stay }) {
                 <br />
                 {review.txt.length > 150 &&
 
-                    <button onClick={()=> setIsModalOpen(true)}> <span className='show-more'>Show more</span><span className='show-more-arrow'><i class="fa-solid fa-angle-right"></i></span> </button>
+                    <button onClick={() => setIsModalOpen(true)}> <span className='show-more'>Show more</span><span className='show-more-arrow'><i class="fa-solid fa-angle-right"></i></span> </button>
                 }
             </div>
         })}
-         <button className="open-modal open-modal-reviews" onClick={() => setIsModalOpen(true)}>Show all {stay.reviews.length} reviews</button>
-         {isModalOpen&&<div className="modal-background" onClick={() => setIsModalOpen(false)}></div>}
+        <button className="open-modal open-modal-reviews" onClick={() => setIsModalOpen(true)}>Show all {stay.reviews.length} reviews</button>
+        {isModalOpen && <div className="modal-background" onClick={() => setIsModalOpen(false)}></div>}
 
 
-            {isModalOpen&&<div className="modal-container modal-reviews-container">
-                <div className='close-button'>  <button onClick={() => setIsModalOpen(false)}> <i class="fa-solid fa-x"></i> </button></div>
-                <div className="scroller">
+        {isModalOpen && <div className="modal-container modal-reviews-container">
+            <div className='close-button'>  <button onClick={() => setIsModalOpen(false)}> <i class="fa-solid fa-x"></i> </button></div>
+            <div className="scroller">
                 {/* className="modal-amenities" */}
-                    <div className='modal-reviews' >
-                        <h2> <i className="fa-sharp fa-solid fa-star"></i>{stay.rating} · {stay.reviews.length} reviews</h2>
-                        <ul>
-                            {updatedReviews.map(review =>
-                                <div className=" review-li">
+                <div className='modal-reviews' >
+                    <h2> <i className="fa-sharp fa-solid fa-star"></i>{stay.rating} · {stay.reviews.length} reviews</h2>
+                    <ul>
+                        {updatedReviews.map((review, index) =>
+                            <div className=" review-li">
                                 <li key={review}>
                                     <div className='review-details'>
-                                      <img src={review.by.imgUrl}/>
-                                      <div>
-                                      <h4>{review.by.fullname}</h4>
-                                      {getTime(review.at)}
-                                      </div>
-                                      </div>
-                                        {/* <p> */}
-                                            {review.txt}
-                                        {/* </p> */}
-                                    </li>
-                                    {/* <hr className="hLine" /> */}
-                                </div>
-                                )}
-                        </ul>
-                    </div>
+                                        <img src={review.by.imgUrl} onError={() => handleImageError(index)} />
+                                        <div>
+                                            <h4>{review.by.fullname}</h4>
+                                            {getTime(review.at)}
+                                        </div>
+                                    </div>
+                                    {/* <p> */}
+                                    {review.txt}
+                                    {/* </p> */}
+                                </li>
+                                {/* <hr className="hLine" /> */}
+                            </div>
+                        )}
+                    </ul>
                 </div>
             </div>
-            }
+        </div>
+        }
     </React.Fragment>
     )
 }
